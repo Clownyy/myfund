522a41e834b9e9cc823b08de3cba49bc29fc5e89
Muhammad Bafaqih <bafaqih555@gmail.com>
2025-10-27 13:05:45 +0700

    login page using deviceId

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 
 export default function Home() {
     return (
-        <Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
             <Login />
         </Suspense>
     );
